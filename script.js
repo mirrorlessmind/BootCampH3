c0d0b44a66bdb1830b9f33b2f660a4bea9a31f94
Cigi <cigi@mirrorlessmind.com>
2021-02-25 20:41:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-//Variables, functions, arrays, conditional statements homework3
-
-// Array of special characters to be included in password
-var specialCharacters = ["$","#","?", "*", "<", "@", "~", "'", "%", "+", ",", "-", "\", ".", "/", ":", ";", "&", "=", ">", "^", "`", "|", "{", "}", "!" ];;
-// Array of numeric characters to be included in password
-var numericCharacters = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-// User input variables 
+//Variables functions arrays conditional statements
+// User decided variables
 var enter;
 var confirmNumber;
 var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
+
+// Array of special characters to be included in password
+specialCharacters = ["$","#","?", "*", "<", "@", "~", "'", "%", "+", ",", "-", "\", ".", "/", ":", ";", "&", "=", ">", "^", "`", "|", "{", "}", "!" ];;
+// Array of numeric characters to be included in password
+numericCharacters = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Array of lowercase characters to be included in password
+lowerCasedCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Array of uppercase characters to be included in password
+upperCasedCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+};
 
 // Choices declared outside the if statement so they can be concatenated
 var choices;
