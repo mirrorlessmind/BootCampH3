4bc598d95a7a4d9b645cb613a5d672ba72a2bad0
Cigi <cigi@mirrorlessmind.com>
2021-02-25 19:24:31 -0600

    Updated
    Added more descriptions. Trying to fix the generate portion to work.

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 //Variables, functions, arrays, conditional statements homework3
 
-//Variables
+//Parameters for password
 // Array of special characters to be included in password
-var specialCharacters = ["!","$","#","?"];
+specialCharacters = ["$","#","?", "*", "<", "@", "~", "'", "%", """, "+", ",", "-", "\", ".", "/", ":", ";", "&", "=", ">", "^", "_", "`", "|", "{", "}", "!" ];
+// Array of lowercase characters to be included in password
+lowerCasedCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Array of uppercase characters to be included in password
+upperCasedCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 // Array of numeric characters to be included in password
 var numericCharacters = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 // User input variables 
 var enter;
-var confirmNumber;
+var confirmNumeric;
 var confirmCharacter;
-var confirmUppercase;
-var confirmLowercase;
+var confirmUppercased;
+var confirmLowercased;
 
 // Choices declared outside the if statement so they can be concatenated
 var choices;
@@ -28,42 +29,33 @@ get.addEventListener("click", function () {
 
 // Start function to generate password
 function generatePassword() {
-        // Asks for length
+        // Asks for length from user
         enter = parseInt(prompt("How many characters between 8 and 128 would you like your password?"));
         // If statement for user validation 
         if (!enter) {
-            alert("This needs a value");
+            alert("My friend enter a value");
         } else if (enter < 8 || enter > 128) {
             // Validates & Start user input
-            
-            enter = parseInt(prompt("You must choose between 8 and 128"));
-
+            enter = parseInt(prompt("Please choose between 8 and 128"));
+            // Continues once input is validated
         } else {
-            // Continues once user input is validated
-            confirmNumber = confirm("Will it contain numbers?");
+            confirmNumeric = confirm("Will it contain numbers?");
             confirmCharacter = confirm("Will it contain special characters?");
-            confirmUppercase = confirm("Will it contain uppercase letters?");
-            confirmLowercase = confirm("Will it contain lowercase letters?");
+            confirmUppercased = confirm("Will it contain uppercase letters?");
+            confirmLowercased = confirm("Will it contain lowercase letters?");
         };
 
-        // Else if for 4 negative options
-        if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+        // Else if for 4 negative options and Choices
+        if (!confirmCharacter && !confirmNumeric && !confirmUppercased && !confirmLowercased) {
             choices = alert("Please choose at least one criteria!");
 
         }
-
-
-
-
-        // If statement with user input prompts to determine choices
-        // Else if for 4 positive options
-        else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
+        // Else if for 4 positive choices
+        else if (confirmCharacter && confirmNumeric && confirmUppercased && confirmLowercased) {
 
             choices = character.concat(character, numericCharacters, upperCasedCharacters, lowerCasedCharacters);
         }
-
-
-        // Else if for 3 positive options
+        // Else if for 3 positive choices
         else if (confirmCharacter && confirmNumber && confirmUppercase) {
             choices = character.concat(number, upperCasedCharacters);
         }
@@ -76,9 +68,7 @@ function generatePassword() {
         else if (confirmNumber && confirmLowercase && confirmUppercase) {
             choices = number.concat(numericCharacters, lowerCasedCharacters, upperCasedCharacters);
         }
-
-
-        // Else if for 2 positive options 
+        // Else if for 2 positive choices 
         else if (confirmCharacter && confirmNumber) {
             choices = character.concat(number);
 
@@ -97,9 +87,7 @@ function generatePassword() {
         } else if (confirmNumber && confirmUppercase) {
             choices = number.concat(upperCasedCharacters);
         }
-
-
-        // Else if for 1 positive option
+        // Else if for 1 positive choice
         else if (confirmCharacter) {
             choices = character;
         }
@@ -113,16 +101,16 @@ function generatePassword() {
             choices = upperCasedCharacters;
         };
 
-        // password variable is an array placeholder for user generated length
+        // Password variable is an array holder for user choice length received assistance
         var password = [];
 
-        // Start random selection variables:
+        // Create random selection variables:
         // Random selection for all variables: 
         for (var i = 0; i < enter; i++) {
             var pickChoices = choices[Math.floor(Math.random() * choices.length)];
             password.push(pickChoices);
         }
-        // This joins the password array and converts it to a string received help
+        // This joins the array and converts it to a string 
         var ps = password.join("");
         UserInput(ps);
         return ps;
@@ -130,18 +118,14 @@ function generatePassword() {
 // This puts the password value into the textbox
 function UserInput(ps) {
     document.getElementById("password").textContent = ps;
-
 }
-
- var copy 
- document.querySelector("#copy");
- copy.addEventListener("click", () => {
+ var copy = document.querySelector("#copy");
+ copy.addEventListener("click", function() {
          copyPassword();
-     });
+});
 // This copies the password value confirmed that it works
-
 function copyPassword() {
     document.getElementById("password").select();
     document.execCommand("Copy");
-    alert("Password copied to clipboard!");
+    alert("Password copied use it wisely!");
 }
